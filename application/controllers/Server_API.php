ee5851e57a8186d4ec8efcdf11bb191d832950c6
dwibagus09 <58162245+dwibagus09@users.noreply.github.com>
2020-06-10 10:32:54 +0700

    ini yang baru

--- application/controllers/Server_API.php
+++ application/controllers/Server_API.php
@@ -1,10 +1,8 @@
 <?php
 if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-// Load the Rest Controller library
-require APPPATH . '/libraries/REST_Controller.php';
 
-class Server_API extends REST_Controller {
+class Server_API extends CI_Controller {
 
     public function __construct() { 
         parent::__construct();
@@ -15,37 +13,15 @@ class Server_API extends REST_Controller {
     
     public function login_post() {
         // Get the post data
-        $username = $this->post('username');
-        $password = $this->post('password');
-        
-        // Validate the post data
-        if(!empty($email) && !empty($password)){
-            
-            // Check if any user exists with the given credentials
-            $con['returnType'] = 'single';
-            $con['conditions'] = array(
-                'username' => $username,
-                'password' => $password,
-                'status' => 1
-            );
-            $user = $this->Siswa_model->getRows($con);
-            
-            if($user){
-                // Set the response and exit
-                $this->response([
-                    'status' => TRUE,
-                    'message' => 'User login successful.',
-                    'data' => $user
-                ], REST_Controller::HTTP_OK);
-            }else{
-                // Set the response and exit
-                //BAD_REQUEST (400) being the HTTP response code
-                $this->response("Wrong email or password.", REST_Controller::HTTP_BAD_REQUEST);
-            }
-        }else{
-            // Set the response and exit
-            $this->response("Provide email and password.", REST_Controller::HTTP_BAD_REQUEST);
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+    
+        if ((empty($username)) || (empty($password))) { 
+        $response = new Siswa_model();
+        $response->success = 0;
+        $response->message = "Kolom tidak boleh kosong"; 
+        die(json_encode($response));
         }
+        $this->Siswa_model->getRows($username,$password);
     }
 }
-    
--- application/models/Siswa_model.php
+++ application/models/Siswa_model.php
@@ -2,53 +2,33 @@
 if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 class Siswa_model extends CI_Model {
-
-    public function __construct() {
-        parent::__construct();
-        
-        // Load the database library
-        $this->load->database();
-        
-        $this->userTbl = 'tb_login';
-    }
-
     /*
      * Get rows from the users table
      */
-    function getRows($params = array()){
-        $this->db->select('*');
-        $this->db->from($this->userTbl);
-        
-        //fetch data by conditions
-        if(array_key_exists("conditions",$params)){
-            foreach($params['conditions'] as $key => $value){
-                $this->db->where($key,$value);
-            }
-        }
-        
-        if(array_key_exists("id",$params)){
-            $this->db->where('id',$params['id']);
-            $query = $this->db->get();
-            $result = $query->row_array();
-        }else{
-            //set start and limit
-            if(array_key_exists("start",$params) && array_key_exists("limit",$params)){
-                $this->db->limit($params['limit'],$params['start']);
-            }elseif(!array_key_exists("start",$params) && array_key_exists("limit",$params)){
-                $this->db->limit($params['limit']);
-            }
-            
-            if(array_key_exists("returnType",$params) && $params['returnType'] == 'count'){
-                $result = $this->db->count_all_results();    
-            }elseif(array_key_exists("returnType",$params) && $params['returnType'] == 'single'){
-                $query = $this->db->get();
-                $result = ($query->num_rows() > 0)?$query->row_array():false;
-            }else{
-                $query = $this->db->get();
-                $result = ($query->num_rows() > 0)?$query->result_array():false;
-            }
-        }
-
-        //return fetched data
-        return $result;
+    /**
+     * 
+     */
+    function getRows($username,$password){
+    $query = $this->db->query("SELECT * FROM tb_login WHERE username='$username' AND password='$password'");
+    if($query->num_rows() > 0){ //jika login sebagai dosen
+        $data=$query->row_array();
+        $this->session->set_userdata('masuk',TRUE);
+        if($data['akses']=='3')
+          { //Akses admin
+        $response = new Siswa_model();
+        $response->message = "Selamat datang ".$data['username'];
+        $response->id = $data['id'];
+        $response->username = $data['username'];
+        die(json_encode($response));
+ 
+                 } else { 
+        $response = new Siswa_model();
+        $response->success = 0;
+        $response->message = "Username atau password salah";
+        die(json_encode($response));
     }
+    
+    mysqli_close();
+    }
+}
+}
